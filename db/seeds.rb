d7f4a190fe81c0485d5f962797e6c9882106c309
Subaru Nakazono <subaru.nakazono.biz@gmail.com>
2025-01-11 08:33:24 +0000

    add book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+books_count = 20
+
+# テストデータをクリア
+Book.destroy_all
+
+# Fakerを使ってランダムなデータを生成
+books_count.times do
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    description: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+end
+
+puts "#{books_count} random books created successfully!"
