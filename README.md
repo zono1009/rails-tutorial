fecc8b75e199bf53b34e74ddb54d4a88ce59c3fd
Subaru Nakazono <subaru.nakazono.biz@gmail.com>
2025-01-11 08:55:12 +0000

    add controller test

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ docker-compose build
 docker-compose run --rm web bundle exec rails db:migrate
 docker-compose run --rm web bundle exec rails db:seed
 docker-compose up
+docker-compose run --rm web bundle exec rails test
 ```
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -6,8 +6,22 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
+    20.times do |i|
+      Book.create(title: "Book #{i}", author: "Author #{i}", description: "Description #{i}")
+    end
+
     get books_url
     assert_response :success
+
+    # レスポンスの内容を解析
+    books = assigns(:books) # コントローラーで設定されたインスタンス変数 @books を取得
+
+    # 取得件数が10件であることを確認
+    assert_equal 10, books.size
+
+    # id順にソートされていることを確認
+    ids = books.map(&:id)
+    assert_equal ids, ids.sort
   end
 
   test "should get new" do
