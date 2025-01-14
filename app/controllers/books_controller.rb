dda40cec9056bdb87f1926557a8130fecb57250a
Subaru Nakazono <subaru.nakazono.biz@gmail.com>
2025-01-14 15:39:51 +0900

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.order(:id).limit(10)
+    @books = Book.order(:id).limit(20)
   end
 
   # GET /books/1 or /books/1.json
